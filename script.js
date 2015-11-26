a8ba37946eda063d6ea83a75857de4fdf08177d7
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-11-26 09:36:09 +0000

    commented url for reject branch

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ if(window.Promise){
       var promise=new Promise(function(resolve,reject){
             var request = new XMLHttpRequest();
             request.open('GET', 'http://api.icndb.com/jokes/random');
+            // request.open('GET', 'http://pepe.pum.es/');
             request.onload = function() {
               if (request.status == 200) {
                 resolve(request.response); //we get the data here.So, resolve the Promise
@@ -11,16 +12,16 @@ if(window.Promise){
                 reject(Error(request.statusText)); //if status is not 200 OK, reject.
               }
             };
-        
+
             request.onerror = function() {
               reject(Error("Error fetching data.")); //error occurred, reject the Promise
             };
-        
+
             request.send(); //send the request
       });
-              
+
       console.log('Asynchronous request made.');
-    
+
       promise.then(function(data){
             console.log('Got data! Promise fulfilled.');
             document.getElementById('joke').innerHTML=JSON.parse(data).value.joke;
